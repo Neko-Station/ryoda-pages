8dc3c6244cf9b3bf5c1aa3cd83c87e36f2a0ae0a
Ryoda <ryoda41@gmail.com>
2025-01-03 22:20:32 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,5 @@ const nextConfig: NextConfig = {
   /* config options here */
     output: "export",
 };
-module.exports = {
-  images: {
-    domains: ['github.com'],
-  },
-};
 
 export default nextConfig;
