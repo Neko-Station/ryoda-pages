90937a5a903086147f7529e299b5febdb368471b
朝猫 <155109251+AsaNekoo@users.noreply.github.com>
2025-01-03 22:43:45 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 
-const imageLoader = ({ src }) => {
+const imageLoader = ({ src: string }) => {
   return `https://github.com/${src}`
 }
 
